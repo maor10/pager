1666f59f3ba49dff221c1aba019f577926753ac2
Maor Kern <kern@adaptive-shield.com>
2020-07-03 14:34:05 +0300

    Add all c files in cpager

--- src/setup.py
+++ src/setup.py
@@ -5,10 +5,9 @@ from setuptools import setup, find_packages, Extension
 
 
 BASE_C_SOURCES_DIRECTORY = os.path.join(os.path.dirname(os.path.abspath(__file__)), "cpager")
+PATHS = glob.glob(BASE_C_SOURCES_DIRECTORY + '/**/*.c', recursive=True)
 
-files = glob.glob(BASE_C_SOURCES_DIRECTORY + '/**/*.c', recursive=True)
-
-module = Extension("cpager", sources=files)
+module = Extension("cpager", sources=PATHS)
 setup(name='pager',
       version='1.0',
       packages=find_packages(),
